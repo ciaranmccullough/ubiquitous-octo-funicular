abb6efd93ee8421b44c7085f65337c7a7f9b925f
ciaranmccullough <59839106+ciaranmccullough@users.noreply.github.com>
2021-11-12 16:00:20 +0000

    add link to about

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from '../logo.svg';
 import "../App.css";
+import { Link } from "react-router-dom";
+
 
 export default function Home () {
     return (
@@ -18,6 +20,7 @@ export default function Home () {
           >
             Learn React
           </a>
+          <Link to='/about'>About</Link>
         </header>
         <div
           id="consent_blackbar"
